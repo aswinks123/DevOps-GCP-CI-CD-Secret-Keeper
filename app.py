b93ceefa9fcfc072540058196423a7a1e4b8949b
Aswin KS <108337342+aswinks123@users.noreply.github.com>
2023-10-21 17:48:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import pyperclip
 app = Flask(__name__)
 
 # Connect to the Redis server
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host='10.16.39.91', port=6379, db=0)
 
 # Define a Redis key for storing the data-to-code mapping
 REDIS_MAPPING_KEY = "data_to_code_mapping"
