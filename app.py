629d7fbbdcc1d01c3a53a12239aea157e85ef371
aswin <aswinks123>
2023-10-27 19:04:24 -0400

    build spec added

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from pywebio.session import run_js, set_env
 app = Flask(__name__)
 
 # Connect to the Redis server
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host='10.232.165.227', port=6379, db=0)
 
 # Define a Redis key for storing the data-to-code mapping
 REDIS_MAPPING_KEY = "data_to_code_mapping"
